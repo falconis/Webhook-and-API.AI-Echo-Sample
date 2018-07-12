5b65d0ceb8a39c64c3fa49bc4cb477045c55d48f
Vishal <panivishal17@gmail.com>
2018-07-13 01:09:55 +0530

    more minor changes

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ restService.post("/echo", function(req, res) {
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-    "fulfillmentTest": speech,
+    "fulfillmentTest": " ",
     "fulfillmentMessages": [{"text": {"text": [speech]}}],
     "source":""
   });
